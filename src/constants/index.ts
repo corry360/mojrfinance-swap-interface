f3b414cb63f5cb2cacf9aeed4001b2dc467e8978
corry360 <97869517+corry360@users.noreply.github.com>
2022-01-21 00:04:32 -0800

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 import { ChainId, JSBI, Percent, Token, WETH } from '@pancakeswap-libs/sdk'
 
-export const ROUTER_ADDRESS = '0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F'
+export const ROUTER_ADDRESS = '0xa53ff84a28144f52894c2d07e207a95a8691dd5e'
 
 // a list of tokens by chain
 type ChainTokenList = {
