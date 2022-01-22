101876b849bfb058db9e220cea27fa5f46b1cec2
corry360 <97869517+corry360@users.noreply.github.com>
2022-01-22 19:02:33 +0300

    Update config.ts

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -31,48 +31,81 @@ const config: MenuEntry[] = [
     icon: 'PoolIcon',
     href: 'https://pedantic-banach-b6c749.netlify.app/pools',
   },
-  {
-    label: 'NFT (coming),
-    href: `${nftsBaseUrl}`,
-    icon: 'Nft',
-    items: [
-      {
-        label: 'Overview',
-        href: `${nftsBaseUrl}`,
-      },
-      {
-        label: 'Collections',
-        href: `${nftsBaseUrl}/collections`,
-      },
-      {
-        label: 'Activity',
-        href: `${nftsBaseUrl}/activity`,
-      },
-    ],
-  },
-  {
-    label: 'More',
-    icon: 'MoreIcon',
-    items: [
-      {
-        label: 'Github',
-        href: 'https://github.com/',
-      },
-      {
-        label: 'Docs',
-        href: 'https://mozartfi/',
-      },
-     
-    ],
-  },
     {
-        label: t('IFO (coming)'),
-        href: '/ifo',
+    label: 'Trade',
+    icon: 'TradeIcon',
+    initialOpenState: true,
+    status: {
+      text: 'MIGRATE',
+      color: 'warning',
+    },
+    items: [
+      {
+        label: 'LP Migration',
+        href: '/migrate',
       },
       {
-        label: t('Voting'),
-        href: '/voting',
+        label: 'Exchange',
+        href: 'https://exchange.pancakeswap.finance/#/swap',
       },
+      {
+        label: 'Liquidity',
+        href: 'https://exchange.pancakeswap.finance/#/pool',
+      },
+      {
+        label: 'V1 Liquidity (Old)',
+        href: '/pool',
+      },
+    ],
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: 'https://pancakeswap.finance/farms',
+  },
+  {
+    label: 'Pools',
+    icon: 'PoolIcon',
+    href: 'https://pancakeswap.finance/pools',
+  },
+  {
+    label: 'Prediction (BETA)',
+    icon: 'PredictionsIcon',
+    href: 'https://pancakeswap.finance/prediction',
+  },
+  {
+    label: 'Lottery',
+    icon: 'TicketIcon',
+    href: 'https://pancakeswap.finance/lottery',
+  },
+  {
+    label: 'Collectibles',
+    icon: 'NftIcon',
+    href: 'https://pancakeswap.finance/nft',
+  },
+  {
+    label: 'Team Battle',
+    icon: 'TeamBattleIcon',
+    href: 'https://pancakeswap.finance/competition',
+  },
+  {
+    label: 'Teams & Profile',
+    icon: 'GroupsIcon',
+    items: [
+      {
+        label: 'Leaderboard',
+        href: 'https://pancakeswap.finance/teams',
+      },
+      {
+        label: 'Task Center',
+        href: 'https://pancakeswainance/profile/tasks',
+      },
+      {
+        label: 'Your Profile',
+        href: 'https://pancakeswapnance/profile',
+      },
+    ],
+  },
   {
     label: 'Audit by IB',
     icon: 'TicketIcon',
